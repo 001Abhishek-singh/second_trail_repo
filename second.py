7bf1be519aea27b22d078dd2f618e7b6d3dc8c85
Abhishek <001abhishek287@gmail.com>
2024-07-08 23:39:00 +0530

    basic python program

---
+++ second.py
@@ -0,0 +1,7 @@
+num1 = input("enter your first number: ")
+num2 = input("enter your second number: ")
+result = int(num1) + int(num2)
+if result >= 20:
+    print("yes you are correct")
+else:
+    print('no you are not correct')
